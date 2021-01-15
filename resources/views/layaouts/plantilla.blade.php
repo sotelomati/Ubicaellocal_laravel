c28fb4bec723df027d9a24a869bccac00ff0a32f
Day <daianrene99@gmail.com>
2021-01-14 22:42:47 -0300

    ofertas

--- resources/views/layaouts/plantilla.blade.php
+++ resources/views/layaouts/plantilla.blade.php
@@ -26,7 +26,7 @@
           <ul class="navbar-nav ms-auto me-5">
 
             <li class="nav-item " >
-              <a href="#" class="nav-link">
+              <a href="{{url('/ofertas')}}" class="nav-link">
                 Ofertas
               </a>
             </li>
@@ -57,9 +57,8 @@
 
     @yield("body")
 
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"    crossorigin="anonymous">
+    </script>  
     @yield("pie")
     
 
--- resources/views/ofertas.blade.php
+++ resources/views/ofertas.blade.php
@@ -8,5 +8,58 @@
 
 @section("body")
 
+    <div class="container mt-3 mb-3">
+    
+        <div class="row justify-content-center">
+        
 
+            <div class="spinner-border text-primary ms-5" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-border text-secondary ms-5" role="status">
+             <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-border text-success ms-5" role="status">
+             <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-border text-danger ms-5" role="status">
+             <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-border text-warning ms-5" role="status">
+             <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-border text-info ms-5" role="status">
+             <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-border text-dark ms-5" role="status">
+             <span class="visually-hidden">Loading...</span>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container ">
+
+        <div id="carouselExampleFade" class="carousel slide carousel-fade " data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="https://i.ytimg.com/vi/I_EB6rSG7iQ/maxresdefault.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://pbs.twimg.com/media/EDBOx6NWsAAMDf9.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item ">
+                <img src="https://i.ytimg.com/vi/Cx9zYKkECwg/hqdefault.jpg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </a>
+        </div>
+    </div>
 @endsection
